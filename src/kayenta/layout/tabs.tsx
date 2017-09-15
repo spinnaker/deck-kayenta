c3bcc8552b8a2939434fc54a350d904ba8dfcf88
Daniel Peach <dpeach@google.com>
2017-09-15 09:01:40 -0400

    feat: generic tabs components

---
+++ src/kayenta/layout/tabs.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+export interface ITabsProps {
+  children: any;
+}
+
+export function Tabs({ children }: ITabsProps) {
+  return (
+    <ul className="tabs-basic list-unstyled">
+      {children}
+    </ul>
+  );
+}
+
+export interface ITabProps {
+  selected: boolean;
+  children: JSX.Element | JSX.Element[];
+  onClick: Function
+}
+
+export function Tab({ selected = false, children }: ITabProps) {
+  return (
+    <li className={selected ? 'selected' : ''}>
+      {children}
+    </li>
+  );
+}
