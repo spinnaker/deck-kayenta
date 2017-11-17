b4b05160bc9d95f61cc96b45ffc2c3c2d366fd01
Daniel Peach <dpeach@google.com>
2017-11-17 14:18:43 -0500

    feat: live canary run service

---
+++ src/kayenta/domain/ICanaryExecutionStatusResult.ts
@@ -0,0 +1,30 @@
+import { ICanaryJudgeResult } from './ICanaryJudgeResult';
+import { ICanaryClassifierThresholdsConfig, ICanaryConfig } from './ICanaryConfig';
+
+export interface ICanaryExecutionStatusResult {
+  complete: boolean;
+  metricSetPairListId: string;
+  status: string;
+  result: ICanaryResult;
+  stageStatus: { [key: string]: string };
+}
+
+export interface ICanaryResult {
+  judgeResult: ICanaryJudgeResult;
+  config: ICanaryConfig;
+  canaryExecutionRequest: ICanaryExecutionRequest;
+}
+
+export interface ICanaryExecutionRequest {
+  thresholds: ICanaryClassifierThresholdsConfig;
+  controlScope: ICanaryScope;
+  experimentScope: ICanaryScope;
+}
+
+export interface ICanaryScope {
+  scope: string;
+  region: string;
+  start: string;
+  end: string;
+  step: number;
+}
