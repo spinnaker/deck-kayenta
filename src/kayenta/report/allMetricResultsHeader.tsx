b468443acb68037028751975f2a06f0e3952efc9
Daniel Peach <dpeach@google.com>
2017-12-08 14:25:54 -0500

    feat: add top-level report score

--- src/kayenta/report/allMetricResultsHeader.tsx
+++ src/kayenta/report/allMetricResultsHeader.tsx
@@ -1,25 +1,25 @@
 import * as React from 'react';
 import * as classNames from 'classnames';
 
-import ScoreArrow from './scoreArrow';
+import HeaderArrow from './headerArrow';
 
 export interface IAllMetricResultsHeader {
   onClick: () => void;
-  className?: string;
+  className: string;
 }
 
 /*
 * Clickable header for all metric results.
 */
 export default ({ className, onClick }: IAllMetricResultsHeader) => (
-  <section className={classNames(className, 'clickable')}>
+  <section className={className}>
     <div
       onClick={onClick}
       className={classNames('clickable', 'text-center', 'all-metric-results-header')}
     >
-      <h3 className="heading-3 uppercase label">all</h3>
-      <ScoreArrow className="outer" borderTopColor="var(--color-titanium)"/>
-      <ScoreArrow className="inner" borderTopColor="var(--color-alabaster)"/>
+      <h3 className="heading-3 label">ALL</h3>
+      <HeaderArrow className="outer" arrowColor="var(--color-titanium)"/>
+      <HeaderArrow className="inner" arrowColor="var(--color-alabaster)"/>
     </div>
   </section>
 );
---
+++ src/kayenta/report/score.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { round } from 'lodash';
+import { ScoreClassificationLabel } from '../domain/ScoreClassificationLabel';
+import { ICanaryJudgeScore } from '../domain/ICanaryJudgeResult';
+import { CanaryScore } from '../components/canaryScore';
+
+export interface ICanaryJudgeScoreProps {
+  score: ICanaryJudgeScore;
+}
+
+export default ({ score }: ICanaryJudgeScoreProps) => (
+  <CanaryScore
+    className="score-report"
+    score={round(score.score, 2)}
+    result={mapKayentaToACA(score).result}
+    health={mapKayentaToACA(score).health}
+    inverse={false}
+  />
+);
+
+// TODO(dpeach): don't use ACA statuses in the CanaryScore component.
+const mapKayentaToACA = (score: ICanaryJudgeScore): { health: string, result: string } => {
+  if (score.classification === ScoreClassificationLabel.Pass) {
+    return { health: null, result: 'success' };
+  } else if (score.classification === ScoreClassificationLabel.Fail) {
+    return { health: 'unhealthy', result: null };
+  } else {
+    return { health: null, result: null };
+  }
+};
